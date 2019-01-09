2e98f1c9c242dde202b5e375761d80e0222fd43e
mongkonchai buayai <mongkonchai_kk@hotmail.com>
2019-01-09 18:57:21 +0700

    kk

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,38 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
+const livereload = require('gulp-livereload');
 
-gulp.task('default', function(){
+gulp.task('eslint', function(){
   //validate
   gulp.src(['src/*.js'])
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError());
-
-   //transfer es6 to es5 js
-    gulp.src('src/*.js')
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-    .pipe(gulp.dest('dist'));
 });
+
+gulp.task('babel', function(){
+  //transfer es6 to es5 js
+   gulp.src('src/*.js')
+   .pipe(babel({
+     presets: ['@babel/env']
+   }))
+   .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['eslint', 'babel']);
+
+// gulp.task('default', function(){
+//   //validate
+//   gulp.src(['src/*.js'])
+//   .pipe(eslint())
+//   .pipe(eslint.format())
+//   .pipe(eslint.failAfterError());
+//
+//    //transfer es6 to es5 js
+//     gulp.src('src/*.js')
+//     .pipe(babel({
+//       presets: ['@babel/env']
+//     }))
+//     .pipe(gulp.dest('dist'));
+// });
