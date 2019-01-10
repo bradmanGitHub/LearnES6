712d692e74ac0229a04b2d6812b0c815e57a089d
mongkonchai buayai <mongkonchai_kk@hotmail.com>
2019-01-10 13:42:12 +0700

    kk

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,25 @@ const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 const livereload = require('gulp-livereload');
 
-gulp.task('eslint', function(){
-  //validate
+gulp.task('eslint', function(done){
   gulp.src(['src/*.js'])
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError());
+  done();
 });
 
-gulp.task('babel', function(){
-  //transfer es6 to es5 js
-   gulp.src('src/*.js')
-   .pipe(babel({
-     presets: ['@babel/env']
-   }))
-   .pipe(gulp.dest('dist'));
+gulp.task('babel', function(done){
+  gulp.src('src/*.js')
+  .pipe(babel({
+    presets: ['@babel/env']
+  }))
+  .pipe(gulp.dest('dist'));
+  done();
 });
 
-gulp.task('default', ['eslint', 'babel']);
-
+//gulp.task('default', ['eslint','babel']);
+gulp.task('default', gulp.series('eslint', 'babel'));
 // gulp.task('default', function(){
 //   //validate
 //   gulp.src(['src/*.js'])
